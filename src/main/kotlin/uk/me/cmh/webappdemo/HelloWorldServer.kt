771e09b43feea6bf678522fad7f77a25c727c8c1
cmh-dev <57900419+cmh-dev@users.noreply.github.com>
2019-12-19 13:38:07 +0000

    Change of text output from web-app

--- src/main/kotlin/uk/me/cmh/webappdemo/HelloWorldServer.kt
+++ src/main/kotlin/uk/me/cmh/webappdemo/HelloWorldServer.kt
@@ -8,5 +8,5 @@ import org.http4k.server.asServer
 
 fun main() {
     val port :Int = System.getenv("PORT")?.toInt() ?: 8080
-    { request: Request -> Response(OK).body("Hello Big Wide World!") }.asServer(Jetty(port)).start()
+    { request: Request -> Response(OK).body("Hello Big Wide Enormous World!") }.asServer(Jetty(port)).start()
 }
