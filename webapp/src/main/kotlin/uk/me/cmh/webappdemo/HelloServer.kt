9ff17bae90c3fea09edc28b091a83e202ab1a3ae
cmh-dev <57900419+cmh-dev@users.noreply.github.com>
2019-12-24 11:13:30 +0000

    Code clean-up

--- webapp/src/main/kotlin/uk/me/cmh/webappdemo/HelloServer.kt
+++ webapp/src/main/kotlin/uk/me/cmh/webappdemo/HelloServer.kt
@@ -1,10 +1,8 @@
 package uk.me.cmh.webappdemo
 
 import org.http4k.core.Method
-import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
-import org.http4k.routing.RoutingHttpHandler
 import org.http4k.routing.bind
 import org.http4k.routing.routes
 import org.http4k.server.Jetty
